f5399b50d70a6bba0a63c13cf6f7c77fd74f419f
Vinay Bhat <vinaybhat001@gmail.com>
2015-07-30 02:24:21 +0530

    small error

--- ffgif.rb
+++ ffgif.rb
@@ -9,7 +9,7 @@ end
 def generate_gif(vid, start_sec, total_sec, fps = 10, scale = 600, output_name = "output")
   system("ffmpeg -y -ss #{start_sec} -t #{total_sec} -i #{vid} -vf fps=#{fps},scale=#{scale}:-1:flags=lanczos,palettegen palette.png")
   system("ffmpeg -ss #{start_sec} -t #{total_sec} -i #{vid} -i palette.png -filter_complex \"fps=#{fps},scale=#{scale}:-1:flags=lanczos[x];[x][1:v]paletteuse\" #{output_name}.gif")
-  system("rm pallete.png")
+  system("rm palette.png")
 end
 
 filename = ARGV[0]
