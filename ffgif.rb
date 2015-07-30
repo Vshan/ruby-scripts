47425d238b2e0b4c20cda8d159ea58e4d7080e9f
Vinay Bhat <vinaybhat001@gmail.com>
2015-07-30 16:54:04 +0530

    validations

--- ffgif.rb
+++ ffgif.rb
@@ -6,6 +6,10 @@ def convert_str_to_time(start)
   seconds = second + (minute*60) + (hour*60*60)
 end
 
+def validate_time(time)
+  time.split(":").size == 3
+end
+
 def generate_gif(vid, start_sec, total_sec, fps = 10, scale = 600, output_name = "output")
   system("ffmpeg -y -ss #{start_sec} -t #{total_sec} -i #{vid} -vf fps=#{fps},scale=#{scale}:-1:flags=lanczos,palettegen palette.png")
   system("ffmpeg -ss #{start_sec} -t #{total_sec} -i #{vid} -i palette.png -filter_complex \"fps=#{fps},scale=#{scale}:-1:flags=lanczos[x];[x][1:v]paletteuse\" #{output_name}.gif")
@@ -19,11 +23,15 @@ fps = ARGV[3]
 scale = ARGV[4]
 output_name = ARGV[5]
 
-start_in_sec = convert_str_to_time(start)
-total_secs = convert_str_to_time(fin) - convert_str_to_time(start)
+if validate_time(start) && validate_time(fin)
+  start_in_sec = convert_str_to_time(start)
+  total_secs = convert_str_to_time(fin) - convert_str_to_time(start)
 
-if fps && scale && output_name
-  generate_gif(filename, start_in_sec, total_secs, fps, scale, output_name)
+  if fps && scale && output_name
+    generate_gif(filename, start_in_sec, total_secs, fps, scale, output_name)
+  else
+    generate_gif(filename, start_in_sec, total_secs)
+  end
 else
-  generate_gif(filename, start_in_sec, total_secs)
+  puts "Sorry, start time and end time could not be parsed. Please ensure they're in hh:mm:ss format."
 end
